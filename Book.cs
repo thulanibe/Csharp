0b851ea3621fb11f46bc352be7c79b5426e90d1c
mhlanga <brucethulani6@gmail.com>
2022-02-03 23:49:15 +0200

    Calculator

---
+++ Book.cs
@@ -0,0 +1,30 @@
+using  System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        static void Main(string[]args)
+        {
+          var book = new Book();
+          var grades = new List<double>() {12,7,10,3,6,11,4.1};
+          grades.Add(56.1);
+          
+
+          var result = 0.0;
+          foreach (var number in grades)
+          {
+              result += number ;
+          }
+               result /= grades.Count;
+               Console.WriteLine($"The average grade is {result:N1}");
+             
+            
+        }
+
+    }
+}
+
+    
+        
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+           var numbers = new double [3];
+          numbers[0] = 12.7;
+          numbers[1] = 10.3;
+          numbers[2] = 6.11;
+
+          var result = numbers[0];
+          result = result + numbers [1];
+          result =  result + numbers [2];
+            
+            Console.WriteLine(result);
+
+            if(args.Length>0)
+            {
+            Console.WriteLine($"Hello ,{args[0]}!");
+            }
+            else
+            {
+             Console.WriteLine("Hello");
+            }
+        }
+
+    }
+}
+
+    
+        
